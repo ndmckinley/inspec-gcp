60d546e0b0beb55520e2bdca33f277681000d7fd
Sam Levenick <slevenick@google.com>
2018-11-05 22:34:53 +0000

    Integration tests

--- test/integration/inspec-mm/controls/google_compute_regions.rb
+++ test/integration/inspec-mm/controls/google_compute_regions.rb
@@ -1,4 +1,3 @@
-# The license inside this block applies to this file.
 # Copyright 2017 Google Inc.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +11,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-source 'https://rubygems.org'
-gem 'bundle'
-gem 'inspec', '>= 3.0.0'
-gem 'rubocop'
+title 'Test GCP regions plural resource.'
+
+control 'gcp-regions-1.0' do
+  impact 1.0
+  title 'GCP Region plural test'
+  describe google_compute_regions(project: attribute('project_name')) do
+    it { should exist }
+    its('names') { should include 'us-west1' }
+    its('names') { should include 'us-east4' }
+  end
+end
