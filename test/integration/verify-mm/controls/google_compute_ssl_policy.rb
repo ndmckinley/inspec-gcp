561bf07b7413f62e29296d954ea9843b82c021cd
Sam Levenick <slevenick@google.com>
2018-12-11 21:07:56 +0000

    PR Feedback moving terraform and attributes to single folder

--- test/integration/verify-mm/controls/google_compute_ssl_policy.rb
+++ test/integration/verify-mm/controls/google_compute_ssl_policy.rb
@@ -12,23 +12,19 @@
 #
 # ----------------------------------------------------------------------------
 
-require_relative '../vcr_config'
-
 title 'Test GCP google_compute_ssl_policy resource.'
 
-project_name = attribute('project_name', default: 'graphite-test-sam-chef')
+project_name = attribute(:gcp_project_name, default: '')
 ssl_policy = attribute('ssl_policy', default: {"name"=>"inspec-gcp-ssl-policy", "min_tls_version"=>"TLS_1_2", "profile"=>"CUSTOM", "custom_feature"=>"TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384", "custom_feature2"=>"TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"})
 control 'google_compute_ssl_policy-1.0' do
   impact 1.0
   title 'google_compute_ssl_policy resource test'
 
-  VCR.use_cassette('google_compute_ssl_policy') do
-    describe google_compute_ssl_policy({project: project_name, name: ssl_policy['name']}) do
-      it { should exist }
-      its('min_tls_version') { should cmp ssl_policy['min_tls_version'] }
-      its('profile') { should cmp ssl_policy['profile'] }
-      its('custom_features') { should include ssl_policy['custom_feature'] }
-      its('custom_features') { should include ssl_policy['custom_feature2'] }
-    end
+  describe google_compute_ssl_policy({project: project_name, name: ssl_policy['name']}) do
+    it { should exist }
+    its('min_tls_version') { should cmp ssl_policy['min_tls_version'] }
+    its('profile') { should cmp ssl_policy['profile'] }
+    its('custom_features') { should include ssl_policy['custom_feature'] }
+    its('custom_features') { should include ssl_policy['custom_feature2'] }
   end
 end
