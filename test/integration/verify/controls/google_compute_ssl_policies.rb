33a593790aa3e9f8e6da59a336e1fff5f6377442
Sam Levenick <slevenick@google.com>
2019-01-04 21:47:44 +0000

    Inspec disk resource

--- test/integration/verify/controls/google_compute_ssl_policies.rb
+++ test/integration/verify/controls/google_compute_ssl_policies.rb
@@ -14,8 +14,14 @@
 
 title 'Test GCP google_compute_ssl_policies resource.'
 
-gcp_project_id = attribute(:gcp_project_id, default: '', description: 'The GCP project identifier.')
-ssl_policy = attribute('ssl_policy', default: { "name" => "inspec-gcp-ssl-policy", "min_tls_version" => "TLS_1_2", "profile" => "CUSTOM", "custom_feature" => "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384", "custom_feature2" => "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384" })
+gcp_project_id = attribute(:gcp_project_id, default: 'gcp_project_id', description: 'The GCP project identifier.')
+ssl_policy = attribute('ssl_policy', default: {
+  "name": "inspec-gcp-ssl-policy",
+  "min_tls_version": "TLS_1_2",
+  "profile": "CUSTOM",
+  "custom_feature": "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
+  "custom_feature2": "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
+})
 control 'google_compute_ssl_policies-1.0' do
   impact 1.0
   title 'google_compute_ssl_policies resource test'
@@ -28,7 +34,7 @@ control 'google_compute_ssl_policies-1.0' do
   end
 
   google_compute_ssl_policies(project: gcp_project_id).names.each do |policy_name|
-    describe google_compute_ssl_policy(project: gcp_project_id, name: policy_name) do
+    describe google_compute_ssl_policy({project: gcp_project_id, name: policy_name}) do
       its('min_tls_version') { should eq ssl_policy['min_tls_version'] }
     end
   end
