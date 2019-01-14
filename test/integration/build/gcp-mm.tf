051f8ae0f02645d19ec222d20ff91c2bcc0df110
Sam Levenick <slevenick@google.com>
2019-01-14 20:33:39 +0000

    Add instance group manager resource to InSpec
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- test/integration/build/gcp-mm.tf
+++ test/integration/build/gcp-mm.tf
@@ -10,6 +10,10 @@ variable "subscription" {
   type = "map"
 }
 
+variable "instance_group_manager" {
+  type = "map"
+}
+
 resource "google_compute_ssl_policy" "custom-ssl-policy" {
   name            = "${var.ssl_policy["name"]}"
   min_tls_version = "${var.ssl_policy["min_tls_version"]}"
@@ -28,4 +32,18 @@ resource "google_pubsub_subscription" "default" {
   name                 = "${var.subscription["name"]}"
   topic                = "${google_pubsub_topic.topic.name}"
   ack_deadline_seconds = "${var.subscription["ack_deadline_seconds"]}"
+}
+
+resource "google_compute_instance_group_manager" "gcp-inspec-igm" {
+  project           = "${var.gcp_project_id}"
+  zone              = "${var.gcp_zone}"
+  name              = "${var.instance_group_manager["name"]}"
+  instance_template = "${google_compute_instance_template.default.self_link}"
+  base_instance_name        = "${var.instance_group_manager["base_instance_name"]}"
+  target_pools = []
+  target_size  = 0
+  named_port {
+    name = "${var.instance_group_manager["named_port_name"]}"
+    port = "${var.instance_group_manager["named_port_port"]}"
+  }
 }
