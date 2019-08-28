a73b3f7caf0f366e8fba19d8a8c6a2eb10525ece
Chris Sng <chrissng@users.noreply.github.com>
2019-08-28 18:01:25 +0000

    Allow updating of resource policies in compute_disk

--- .expeditor/update_version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# After a PR merge, Chef Expeditor will bump the PATCH version in the VERSION file.
-# It then executes this file to update any other files/components with that new version.
-#
-
-set -ev
-
-sed -i -r "s/^version\: .*/version\: $(cat VERSION)/" inspec.yml
